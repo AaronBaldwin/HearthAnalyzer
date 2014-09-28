5374f06246d56cd90d0aa10796fe4ae67c8f38e8
Kevin Luo <kwluo90@gmail.com>
2014-09-27 17:15:09 -0700

    Add Deck and FatigueCard objects

---
+++ HearthAnalyzer.Core/Cards/Deck.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HearthAnalyzer.Core.Cards
+{
+    /// <summary>
+    /// Represents a deck of cards
+    /// </summary>
+    public class Deck
+    {
+        private int topDeckIndex = -1;
+        private int fatigueDamage = 0;
+
+        public List<BaseCard> Cards;
+
+        public BaseCard DrawCard()
+        {
+            if (topDeckIndex < 0)
+            {
+                fatigueDamage++;
+                return new FatigueCard(fatigueDamage);
+            }
+
+            BaseCard card = this.Cards[topDeckIndex];
+            this.Cards.RemoveAt(topDeckIndex);
+            topDeckIndex--;
+            return card;
+        }
+
+        /// <summary>
+        /// Adds a card to the current deck
+        /// </summary>
+        /// <param name="card">The card to add</param>
+        /// <remarks>Adds to the end of the deck by default</remarks>
+        public void AddCard(BaseCard card)
+        {
+            this.Cards.Add(card);
+            topDeckIndex++;
+        }
+
+        /// <summary>
+        /// Adds a list of cards to the current deck
+        /// </summary>
+        /// <param name="cards">The list of cards to add</param>
+        /// <remarks>Adds to the end of the deck by default</remarks>
+        public void AddCards(List<BaseCard> cards)
+        {
+            this.Cards.AddRange(cards);
+            topDeckIndex = this.Cards.Count - 1;
+        }
+
+        /// <summary>
+        /// Draws n cards
+        /// </summary>
+        /// <param name="n">The number of cards to draw</param>
+        /// <returns>The list of cards drawn.</returns>
+        public List<BaseCard> DrawCards(int n = 1)
+        {
+            List<BaseCard> cards = new List<BaseCard>();
+            for (int i = 0; i < n; i++)
+            {
+                cards.Add(DrawCard());
+            }
+
+            return cards;
+        }
+
+        /// <summary>
+        /// Shuffle the deck
+        /// </summary>
+        /// <param name="n">The number of times to shuffle the deck</param>
+        public void Shuffle(int n = 5)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < this.Cards.Count; j++)
+                {
+                    var swapIndex = GameEngine.Random.Next(this.Cards.Count);
+                    BaseCard temp = this.Cards[swapIndex];
+                    this.Cards[swapIndex] = this.Cards[j];
+                    this.Cards[j] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ HearthAnalyzer.Core/Cards/FatigueCard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HearthAnalyzer.Core.Cards
+{
+    /// <summary>
+    /// Represents a fatigue card
+    /// </summary>
+    public class FatigueCard : BaseCard
+    {
+        public FatigueCard(int fatigueDamage, int id = -1)
+        {
+            this.Id = id;
+            this.Name = "Fatigue";
+
+            this.FatigueDamage = fatigueDamage;
+        }
+
+        /// <summary>
+        /// The amount of fatigue damage
+        /// </summary>
+        public int FatigueDamage;
+    }
+}
